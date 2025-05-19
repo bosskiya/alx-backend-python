c124e22a1e38538aa62abcf3b5e34a6834347432
binikiya <biniyamkiya3682@gmail.com>
2025-05-19 19:51:05 +0300

    Log Queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(query, *args, **kwargs):
+        print(f"[LOG] Executing SQL Query: {query}")
+        return func(query, *args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
