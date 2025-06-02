ba2eb41114108f87e71e5e4267951c9c92fdae24
binikiya <biniyamkiya3682@gmail.com>
2025-06-02 21:06:30 +0300

    Django Messaging App

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,13 +1,54 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
+import uuid
 
 
-class user(AbstractUser):
+class UserManager(BaseUserManager):
+    """
+    A class to create user and superuser
+    """
+    def create_user(self, email, password=None, **kwargs):
+        if email is None:
+            raise ValueError("User must have an email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **kwargs):
+        if email is None:
+            raise ValueError("User must have an email")
+        user = self.create_user(email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class user(AbstractBaseUser, PermissionsMixin):
     """
     A model description for user
     """
-    pass
+    user_id = models.UUIDField(db_index=True, primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(db_index=True, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 
 class conversation(models.Model):
